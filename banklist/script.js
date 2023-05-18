1258ca8e207205e363f78ded670241bff9cbdac8
Ammad Khaan <97141225+MAmmaadTehseen@users.noreply.github.com>
2023-05-18 14:02:24 +0500

    Update script.js

--- banklist/script.js
+++ banklist/script.js
@@ -1,12 +1,12 @@
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  owner: 'Ammaad Tehseen',
+  movements: [200, 450, 400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Sabir khan',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
@@ -20,7 +20,7 @@ const account3 = {
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'M Moaz',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
